87038ada00eb8cf5a0e3e06e2963a9941e8a2afc
Ateş Balcı <atesbalci@gmail.com>
2017-01-20 21:23:39 +0300

    initial commit

---
+++ Assets/Classes/Emitter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+	private const float EMISSION_RATE = 2;
+
+	public GameObject wavePrefab;
+
+	private float timer;
+	
+	void Start () {
+		timer = 0;
+	}
+	
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer <= 0) {
+			Instantiate(wavePrefab, transform.position, Quaternion.identity);
+			timer = EMISSION_RATE;
+		}
+	}
+}
---
+++ Assets/Classes/Wave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+	private const float TARGET_SIZE = 50;
+	private const float SPEED = 5;
+
+	private float currentSize;
+
+	void Start () {
+		currentSize = 0;
+	}
+	
+	void Update () {
+		currentSize += Time.deltaTime * 5;
+		transform.localScale = new Vector3(currentSize, currentSize, currentSize);
+		if (currentSize >= TARGET_SIZE)
+			Destroy(gameObject);
+	}
+}
